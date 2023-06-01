e1f950b853596f7410c9be8137b6396706498744
d876dk <44205938+d876dk@users.noreply.github.com>
2023-06-01 15:39:03 -0500

    Add files via upload

--- НПСО/js/script.js
+++ НПСО/js/script.js
@@ -7,12 +7,30 @@ $(document).ready(function () {
         autoplay: true,
         autoplaySpeed: 6000,
     });
-    const menu_item = $(".header__menu-item");
-    for (let i = 0; i < menu_item.length; i++) {
-        if (menu_item[i].children[1]) {
-            menu_item[i].children[0].style["border-radius"] = "10px 10px 0 0"
+    const header_item = $(".header__menu-item");
+    const header_dropdown = $(".header__dropdown");
+    console.log(header_item)
+
+    for (let i = 0; i < header_item.length; i++) {
+        console.log(header_item[i])
+        if (header_item[i].children[1]) {
+            header_item[i].children[0].style["border-radius"] = "10px 10px 0 0";
         }
     }
+    //$('header__menu-btn').hover(
+    //    function () {
+    //        console.log($(this));
+    //    }
+    //)
+    $('.header__dropdown-btn').hover(
+        function () {
+            $($(this)[0].parentElement.parentElement.children[0]).addClass("active");
+        },
+        function () {
+            $($(this)[0].parentElement.parentElement.children[0]).removeClass("active");
+        }
+
+    );
 });
 
 
